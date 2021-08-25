a575ead6b97c266aef76d5d7e7c06789c1637086
Faadhil Ahamat <faadhil.aa@gmail.com>
2021-08-25 23:41:55 +0000

    created setupInput function and handleUserInput function

--- play.js
+++ play.js
@@ -1,9 +1,27 @@
 const net = require("net");
 const connect = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+    console.log("Game Ended!");
+    process.exit();
+  }
+};
+
 
 console.log("Connecting ...");
 connect();
+setupInput();
 
 // "Move: up" - move up one square (unless facing down)
 // "Move: down" - move down one square (unless facing up)
